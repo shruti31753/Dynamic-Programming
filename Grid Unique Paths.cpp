f494cc4b09930336fe6d64e48af01d583cb44997
Shruti Shivhare <133192473+shruti31753@users.noreply.github.com>
2024-03-05 18:00:41 +0530

    Create Grid Unique Paths.cpp

---
+++ Grid Unique Paths.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+int f(int m, int n, vector<vector<int>>&dp){
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			if(i==0 && j==0){
+				dp[i][j] = 1;
+				continue;
+			}
+			int up=0;
+			int left = 0;
+
+			if(i>0)
+				up = dp[i-1][j];
+			if(j>0)
+				left = dp[i][j-1];
+
+			dp[i][j] = left + up;
+		}
+	}
+	return dp[m-1][n-1];
+}
+int uniquePaths(int m, int n) {
+	vector<int>prev(n, 0);
+	for(int i=0;i<m;i++){
+		vector<int>temp(n, 0);
+
+		for(int j=0;j<n;j++){
+			if(i==0 && j==0){
+				temp[j] = 1;
+				continue;
+			}
+			int up=0;
+			int left=0;
+
+			if(i>0)
+				up = prev[j];
+			if(j>0)
+				left = temp[j-1];
+			
+			temp[j] = up+left;
+		}
+		prev = temp;
+	}
+
+	return prev[n-1];
+	// Write your code here.
+}
